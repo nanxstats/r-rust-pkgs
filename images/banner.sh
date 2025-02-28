32e6651a889081d540d88ebce239276ca05360c7
Nan Xiao <me@nanx.me>
2025-02-28 16:53:40 -0500

    Use JetBrains Mono

--- images/banner.sh
+++ images/banner.sh
@@ -4,10 +4,10 @@ magick -size 2048x734 \
   -define gradient:angle=330 \gradient:#028cf3-#2feaa8 \
   -gravity center \
   -pointsize 100 \
-  -font 'Cascadia-Code-SemiBold' \
+  -font 'JetBrains-Mono-Bold' \
   -fill white \
   -annotate +0-80 'r-rust-pkgs' \
   -pointsize 40 \
-  -font 'Cascadia-Code-SemiBold' \
+  -font 'JetBrains-Mono-Regular' \
   -annotate +0+60 'Performance · Reliability · Productivity' \
   png:- | pngquant - --force --output images/banner.png
